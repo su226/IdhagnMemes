7257cdba2a6d4f665edf80b3f1f877d3050ff33e
su226 <thesu226@outlook.com>
2025-12-04 00:25:04 +0800

    feat: ori

---
+++ README.md
@@ -0,0 +1,7 @@
+# IdhagnMemes
+
+从 [IdhagnBot](https://github.com/su226/IdhagnBot) 里分离出来的梗图，用于 [meme-generator](https://github.com/MemeCrafters/meme-generator) 加载。 
+
+[--> 表情列表 <--](https://github.com/su226/IdhagnMemes/wiki/%E8%A1%A8%E6%83%85%E5%88%97%E8%A1%A8)
+
+[--> 如何加载表情 <--](https://github.com/MemeCrafters/meme-generator/wiki/%E5%8A%A0%E8%BD%BD%E5%85%B6%E4%BB%96%E8%A1%A8%E6%83%85)
---
+++ src/idhagnmemes/ori/__init__.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+from io import BytesIO
+from pathlib import Path
+
+from meme_generator import add_meme
+from meme_generator.meme import MemeArgsModel
+from pil_utils import BuildImage
+
+img_dir = Path(__file__).parent / "images"
+
+
+def ori(images: list[BuildImage], texts: list[str], args: MemeArgsModel) -> BytesIO:
+    return (
+        BuildImage.open(img_dir / "0.png")
+        .paste(
+            images[0].convert("RGBA").circle().resize((100, 100)),
+            (305, 222),
+            alpha=True,
+        )
+        .save_jpg()
+    )
+
+
+add_meme(
+    "ori",
+    ori,
+    min_images=1,
+    max_images=1,
+    keywords=["ori", "拥抱光明"],
+    date_created=datetime(2022, 11, 11),
+    date_modified=datetime(2025, 12, 4),
+)
